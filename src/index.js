ddfa5ad721dac2979789c9c090397e95397f3e77
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-03 13:21:24 -0500

    Modules link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import $ from 'jquery';
 import 'bootstrap';
-
-
 import './index.scss';
+import './components/biography';
+import './components/contact';
+import './components/projects';
+import './components/technology';
+import './data/projectsData';
 
 $('#test').on('click', () => {
   $('#words').append('<p>Test</p>');
