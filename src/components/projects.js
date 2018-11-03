5990d4ba6ab51dddd00c081e26881199befb10de
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-03 13:54:34 -0500

    axios complete, projects.json successfuly logs to json

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,1 +1,10 @@
-console.log('projects');
+// import $ from 'jquery';
+import getProjects from '../data/projectsData';
+
+getProjects.getProjects()
+  .then((data) => {
+    console.log(data.data.projects, data);
+  })
+  .catch((error) => {
+    console.error({ error });
+  });
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,1 +1,5 @@
-console.log('projectsdata');
+import axios from 'axios';
+
+const getProjects = () => axios.get('http://localhost:3003/projects');
+
+export default { getProjects };
