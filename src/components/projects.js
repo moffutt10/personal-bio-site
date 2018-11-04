0ea12dc16df7644cdf5ee3edca52001956f72de4
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-03 22:35:16 -0500

    object is not a function blah blah blah

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,34 @@
-// import $ from 'jquery';
-import getProjects from '../data/projectsData';
+import $ from 'jquery';
+import projectData from '../data/projectsData';
+import 'bootstrap';
 
-getProjects.getProjects()
-  .then((data) => {
-    console.log(data.data.projects, data);
-  })
-  .catch((error) => {
-    console.error({ error });
+const writeProjects = (projects) => {
+  let domString = '';
+  projects.forEach((project) => {
+    domString += `
+    <div id='${project.title}' class='card'>
+      <img class='card-img-top' src='${project.screenshot}
+      <div class="card-body">
+          <h5 class="card-title">${project.title}</h5>
+          <h6 class="card-text">${project.description}</h6>
+          <p class="technologies-used">${project.technologiesUsed}</p>
+          <a href="${project.githubUrl}" class="btn btn-secondary d-flex justify-content-center">GitHub</a>
+      </div>
+    </div>`;
+    if (project.available === true) {
+      $('#projectsPage').html(domString);
+    }
   });
+};
+
+const loadProjects = () => {
+  projectData.getProjects()
+    .then((data) => {
+      writeProjects(data.data.projects);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export default { loadProjects };
